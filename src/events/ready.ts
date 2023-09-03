87ca419176894aca2feb3549a1005b6e0b78ded8
Louis Ducruet <lducruet74@gmail.com>
2023-09-03 21:39:07 +0200

    Fix version display

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,12 +1,13 @@
 import {Client, Events, ActivityType} from "discord.js";
 import {BotEvent} from "../types";
 
+
 const event: BotEvent = {
     name: Events.ClientReady,
     once: true,
     execute(client: Client) {
         client.log.info(`Le bot est disponible en tant que ${client.user.tag} (${client.user.id})`, {"type": "Bot Ready", "file": __filename, "dir": __dirname});
-        client.user.setActivity(`Version: ${process.env.npm_package_version}`, { type: ActivityType.Custom });
+        client.user.setActivity(`Version: ${require('../../package.json').version}`, { type: ActivityType.Custom });
     }
 }
 
