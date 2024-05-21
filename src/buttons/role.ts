7eb932656f8582dadd3005088ede8f99a4611822
Louis <lducruet74@gmail.com>
2024-05-21 09:12:45 +0200

    Fix btn disable

--- src/buttons/role.ts
+++ src/buttons/role.ts
@@ -3,7 +3,7 @@ import {ButtonActionMessage} from "../types";
 const command: ButtonActionMessage = {
     name: "role",
     execute: async (interaction) => {
-        const ROLES = ["1173259758065700985", "1173259950240309248", "1173260048856776787", "1173260123507015750", "1173260197679091792"];
+        /* const ROLES = ["1173259758065700985", "1173259950240309248", "1173260048856776787", "1173260123507015750", "1173260197679091792"];
         const ACTIONS = ['asrbd', 'cda1', 'cda2', 'ia', 'wis'];
         const user = await interaction.guild.members.fetch(interaction.user.id);
         const action = interaction.customId.split('_')[1];
@@ -19,7 +19,8 @@ const command: ButtonActionMessage = {
         });
         if (check) return;
         await user.roles.add([ROLES[ACTIONS.indexOf(action)].toString()]);
-        await interaction.reply({content: `${user}, le role \`${action}\` viens d'être ajouté !`, ephemeral: true});
+        await interaction.reply({content: `${user}, le role \`${action}\` viens d'être ajouté !`, ephemeral: true}); */
+        interaction.reply({content: "L'obtention automatique des roles n'est plus disponible, veuillez contacter un administrateur pour obtenir votre role.", ephemeral: true})
     }
 }
 
