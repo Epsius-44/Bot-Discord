badb3f6441788146da89233e21c987d82e2443c9
Louis DUCRUET <louis.ducruet@ecoles-espi.net>
2024-07-10 10:58:16 +0200

    Update logs

--- src/modules/logger.ts
+++ src/modules/logger.ts
@@ -17,30 +17,30 @@ export class Logger {
     log(message: string, context?: Context) {
         this.logtail
             ? this.logtail.log(message, "log", context)
-            : console.log(`[LOG]-${dateToString(new Date())} :`,message);
+            : console.log(`[LOG]-${dateToString(new Date())} :`,message, console);
     }
 
     info(message: string, context?: Context) {
         this.logtail
             ? this.logtail.info(message, context)
-            : console.info(`[INFO]-${dateToString(new Date())} :`,message);
+            : console.info(`[INFO]-${dateToString(new Date())} :`,message, console);
     }
 
     warn(message: string, context?: Context) {
         this.logtail
             ? this.logtail.warn(message, context)
-            : console.warn(`[WARN]-${dateToString(new Date())} :`,message);
+            : console.warn(`[WARN]-${dateToString(new Date())} :`,message, console);
     }
 
     error(message: string, context?: Context) {
         this.logtail
             ? this.logtail.error(message, context)
-            : console.error(`[ERROR]-${dateToString(new Date())} :`,message);
+            : console.error(`[ERROR]-${dateToString(new Date())} :`,message, console);
     }
 
     debug(message: string, context?: Context) {
         this.logtail
             ? this.logtail.debug(message, context)
-            : console.debug(`[DEBUG]-${dateToString(new Date())} :`,message);
+            : console.debug(`[DEBUG]-${dateToString(new Date())} :`,message, console);
     }
 }
