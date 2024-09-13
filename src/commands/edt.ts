01a92592d9b3d6ba0595bc913dd23283d667043b
Louis Ducruet <lducruet74@gmail.com>
2024-09-13 11:26:07 +0200

    add: multidate option in /edt

--- src/commands/edt.ts
+++ src/commands/edt.ts
@@ -5,7 +5,56 @@ import {
   SlashCommandBuilder
 } from "discord.js";
 import AppCommand from "../class/AppCommand.js";
-import { EdtElement, getDay } from "@luzilab/irori-edt";
+import { EdtElement, getDay, getDays } from "@luzilab/irori-edt";
+
+function formatDay(
+  edt: EdtElement[],
+  username: string,
+  date: string
+): EmbedBuilder {
+  const embed = new EmbedBuilder()
+    .setTitle(`Emploi du temps de ${username} - ${date}`)
+    .setDescription(
+      `Récupération: <t:${Math.floor(new Date().getTime() / 1000)}:D>\n :warning: Cette commande ne gère pas les salles multiples et en affiche qu'une :warning:`
+    )
+    .setColor(0x0099ff);
+  const fields: APIEmbedField[] = [];
+  edt.forEach((element: EdtElement) => {
+    fields.push({
+      name: `${element.name}`,
+      value: `- :clock8: ${element.start_time} - ${element.end_time}\n- :school: ${element.room == "NS" ? "???" : element.room}\n- :teacher: ${element.teacher == "" ? "???" : element.teacher}`
+    });
+  });
+  embed.addFields(fields);
+  return embed;
+}
+
+function formatDays(
+  edt: EdtElement[][],
+  username: string,
+  start_date: string,
+  end_date: string
+): EmbedBuilder {
+  const embed = new EmbedBuilder()
+    .setTitle(`Emploi du temps de ${username} - ${start_date} à ${end_date}`)
+    .setDescription(
+      `Récupération: <t:${Math.floor(new Date().getTime() / 1000)}:D>\n :warning: Cette commande ne gère pas les salles multiples et en affiche qu'une :warning:`
+    )
+    .setColor(0x0099ff);
+  const fields: APIEmbedField[] = [];
+  edt.forEach((day: EdtElement[]) => {
+    let day_edt = "";
+    day.forEach((element: EdtElement) => {
+      day_edt += `:clock8: \`${element.start_time} - ${element.end_time}\` | :book: ${element.name} :school: ${element.room == "NS" ? "???" : element.room} :teacher: ${element.teacher == "" ? "???" : element.teacher}\n`;
+    });
+    fields.push({
+      name: `${day[0].date}`,
+      value: day_edt
+    });
+  });
+  embed.addFields(fields);
+  return embed;
+}
 
 export default new AppCommand({
   data: new SlashCommandBuilder()
@@ -26,32 +75,36 @@ export default new AppCommand({
     )
     .addStringOption((option) =>
       option
-        .setName("date")
-        .setDescription("Date que vous souhaitez consulter (DD/MM/YYYY)")
+        .setName("start_date")
+        .setDescription(
+          "Première date que vous souhaitez consulter (DD/MM/YYYY)"
+        )
         .setRequired(true)
+    )
+    .addStringOption((option) =>
+      option
+        .setName("end_date")
+        .setDescription(
+          "Dernière date que vous souhaitez consulter (DD/MM/YYYY)"
+        )
+        .setRequired(false)
     ),
 
   hasSubCommands: false,
   roles: [],
   async execute(interaction): Promise<void> {
     const username = interaction.options.get("username")?.value as string;
-    const date = interaction.options.get("date")?.value as string;
+    const start_date = interaction.options.get("start_date")?.value as string;
+    const end_date = interaction.options.get("end_date")?.value as string;
     try {
-      const edt = getDay(username, date);
-      const embed = new EmbedBuilder()
-        .setTitle(`Emploi du temps de ${username} - ${date}`)
-        .setDescription(
-          `Récupération: <t:${Math.floor(new Date().getTime() / 1000)}:D>\n :warning: Cette commande ne gère pas les salles multiples et en affiche qu'une :warning:`
-        )
-        .setColor(0x0099ff);
-      const fields: APIEmbedField[] = [];
-      edt.forEach((element: EdtElement) => {
-        fields.push({
-          name: `${element.name}`,
-          value: `- :clock8: ${element.start_time} - ${element.end_time}\n- :school: ${element.room == "NS" ? "???" : element.room}\n- :teacher: ${element.teacher == "" ? "???" : element.teacher}`
-        });
-      });
-      embed.addFields(fields);
+      let embed: EmbedBuilder = new EmbedBuilder();
+      if (end_date === undefined) {
+        const edt = getDay(username, start_date);
+        embed = formatDay(edt, username, start_date);
+      } else {
+        const edt = getDays(username, start_date, end_date);
+        embed = formatDays(edt, username, start_date, end_date);
+      }
       interaction.reply({ embeds: [embed], ephemeral: true });
     } catch {
       interaction.reply({
@@ -59,7 +112,7 @@ export default new AppCommand({
         ephemeral: true
       });
       interaction.client.logger.error(
-        `Erreur dans le chargement de l'emploi du temps ${username} pour le ${date}`
+        `Erreur dans le chargement de l'emploi du temps ${username} pour ${start_date} à ${end_date}`
       );
       return;
     }
